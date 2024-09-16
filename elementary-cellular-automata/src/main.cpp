54b00fdd883707bceed29cc1838cc19c3c354177
CRBroughton <crbroughton@posteo.uk>
2024-09-16 22:17:24 +0100

    refactor(elementary-cellular-automata): :recycle: refactor calculateState to parse strings to ints, simplify ruleset logic

--- elementary-cellular-automata/src/main.cpp
+++ elementary-cellular-automata/src/main.cpp
@@ -5,14 +5,9 @@
 #include <cmath>
 
 int calculateState(int a, int b, int c) {
-    if (a == 1 && b == 1 && c == 1) return 1;
-    if (a == 1 && b == 1 && c == 0) return 0;
-    if (a == 1 && b == 0 && c == 1) return 1;
-    if (a == 1 && b == 0 && c == 0) return 1;
-    if (a == 0 && b == 1 && c == 1) return 0;
-    if (a == 0 && b == 1 && c == 0) return 1;
-    if (a == 0 && b == 0 && c == 1) return 1;
-    if (a == 0 && b == 0 && c == 0) return 0;
+    std::vector<int> ruleset = {1, 0, 1, 1, 0, 1, 1, 0};
+    int value = (a << 2) | (b << 1) | c; 
+    return ruleset[7 - value];
 }
 
 void setup(std::vector<int>& cells, int cellSize) {
@@ -30,7 +25,7 @@ int main(void) {
 
     const int WINDOW_WIDTH = 810;
     const int WINDOW_HEIGHT = 810;
-    const int cellSize = 10;
+    const int cellSize = 10 ;
     int y = 0;
     std::vector<int> cells;
 
