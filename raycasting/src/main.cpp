d59884cf3e011cca4ba7385ec6139185267afebd
CRBroughton <crbroughton@posteo.uk>
2024-09-27 21:54:38 +0100

    feat(raycasting): :construction: generate random walls

--- raycasting/src/main.cpp
+++ raycasting/src/main.cpp
@@ -2,19 +2,37 @@
 #include "include/boundary.cpp"
 #include "include/ray.cpp"
 #include "include/particle.cpp"
+#include <vector>
+
+const int screenWidth = 400;
+const int screenHeight = 400;
+Vector2 GetRandomCoord()
+{
+    float x = static_cast<float>(GetRandomValue(0, screenWidth));
+    float y = static_cast<float>(GetRandomValue(0, screenHeight));
+    return {x, y};
+};
 
 int main(void)
 {
-    const int screenWidth = 400;
-    const int screenHeight = 400;
 
     InitWindow(screenWidth, screenHeight, "Raycasting Demo");
-
     SetTargetFPS(60);
 
     Vector2 startPoint = {100, 100};
     Vector2 endPoint = {200, 300};
-    Boundary boundary(startPoint, endPoint);
+    std::vector<Boundary> boundaries;
+
+    // Boundary boundary(startPoint, endPoint);
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        float randomWidth = static_cast<float>(GetRandomValue(5, 20));
+        float randomHeight = static_cast<float>(GetRandomValue(5, 20));
+        Boundary boundary({GetRandomCoord(), GetRandomCoord()});
+        boundaries.push_back(boundary);
+    }
+
     Particle particle({screenWidth / 2, screenHeight / 2});
 
     while (!WindowShouldClose())
@@ -22,10 +40,12 @@ int main(void)
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        boundary.Draw();
+        for (Boundary &boundary : boundaries)
+        {
+            boundary.Draw();
+        };
         particle.show();
-        particle.look(boundary);
+        particle.look(boundaries[1]);
         Vector2 mousePosition = GetMousePosition();
 
         particle.update(mousePosition);
