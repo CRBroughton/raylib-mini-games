130cb50492f0c4ebf19d61d077336a11a0bf602f
CRBroughton <crbroughton@posteo.uk>
2024-09-29 13:14:04 +0100

    feat(menger-sponge-fractal): :construction: write generator function for internal boxes

--- menger-sponge-fractal/src/include/box.cpp
+++ menger-sponge-fractal/src/include/box.cpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "raylib.h"
+#include <cmath>
+#include <vector>
 
 class Box
 {
@@ -11,6 +13,29 @@ public:
     {
         this->position = position;
         this->size = size;
+    }
+
+    std::vector<Box> generate()
+    {
+        std::vector<Box> boxes;
+        for (size_t x = -1; x < 2; x++)
+        {
+            for (size_t y = -1; y < 2; y++)
+            {
+                for (size_t z = -1; z < 2; z++)
+                {
+                    float newSize = this->size / 3;
+                    Box box({
+                                this->position.x + x * newSize,
+                                this->position.y + y * newSize,
+                                this->position.z + z * newSize,
+                            },
+                            newSize);
+                    boxes.push_back(box);
+                }
+            }
+        }
+        return boxes;
     }
 
     void show()
