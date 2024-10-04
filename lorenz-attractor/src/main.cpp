429428b7e407302b3cf313a8afe2083c57d87fc6
CRBroughton <crbroughton@posteo.uk>
2024-10-04 20:38:09 +0100

    feat(lorenz-attractor): :recycle: convert attractor to 3D

--- lorenz-attractor/src/main.cpp
+++ lorenz-attractor/src/main.cpp
@@ -1,26 +1,38 @@
 #include "include/raylib.h"
+#include <vector>
 
 const int screenWidth = 800;
 const int screenHeight = 800;
 
-float x = 00.1;
+float x = 0.1f;
 float y = 0;
 float z = 0;
 
-const float a = 10;
-const float b = 28;
-const float c = 8.0 / 3.0;
-const float scale = 10.0f;
+const float a = 10.0f;
+const float b = 28.0f;
+const float c = 8.0f / 3.0f;
+const float scale = 1.0f;
+
+std::vector<Vector3> points;
+
 int main(void)
 {
     InitWindow(screenWidth, screenHeight, "Lorenz Attractor");
     SetTargetFPS(60);
 
+    Camera3D camera = {0};
+    camera.position = (Vector3){0.0f, 0.0f, 150.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        BeginMode3D(camera);
 
-        float dt = 0.01;
+        float dt = 0.01f;
         float dx = (a * (y - x)) * dt;
         float dy = (x * (b - z) - y) * dt;
         float dz = (x * y - c * z) * dt;
@@ -28,10 +40,14 @@ int main(void)
         y = y + dy;
         z = z + dz;
 
-        int screenX = screenWidth / 2 + x * scale;
-        int screenY = screenHeight / 2 + y * scale;
-        DrawPixel(screenX, screenY, WHITE);
+        points.push_back({x, y, z});
 
+        for (int i = 1; i < points.size(); i++)
+        {
+            DrawLine3D(points[i - 1], points[i], WHITE);
+        }
+
+        EndMode3D();
         EndDrawing();
     }
     CloseWindow();
