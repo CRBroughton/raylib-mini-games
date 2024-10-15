0064c81a16669cc179a4aa12c4a2d9a62db2ab93
CRBroughton <crbroughton@posteo.uk>
2024-10-15 19:12:40 +0100

    feat(phyllotaxis): :sparkles: create the basic pattern

--- phyllotaxis/src/main.cpp
+++ phyllotaxis/src/main.cpp
@@ -1,7 +1,8 @@
 #include "include/raylib.h"
+#include "include/raymath.h"
 
-const int screenWidth = 400;
-const int screenHeight = 400;
+const int screenWidth = 800;
+const int screenHeight = 800;
 
 int main(void)
 {
@@ -9,11 +10,21 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Phyllotaxis");
     SetTargetFPS(60);
 
+    int n = 0;
+    int c = 20;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        float angle = n * 137.5;
+        float radius = c * sqrt(n);
+
+        float x = radius * cos(angle) + screenWidth / 2;
+        float y = radius * sin(angle) + screenHeight / 2;
+
+        DrawEllipse(x, y, 8, 8, WHITE);
+        n++;
 
         EndDrawing();
     }
